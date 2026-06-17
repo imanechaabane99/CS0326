a469c4ab962f07b0a6d34cb357069d0498ef2c36
Imane Chaabane <imanechaabane617@gmail.com>
2026-06-17 17:17:52 +0200

    Update perimetro.py

--- UNIT_1/S2/L3/perimetro.py
+++ UNIT_1/S2/L3/perimetro.py
@@ -4,15 +4,15 @@ figura=int(input("inserisci una figura geometrica 1=per quadrato 2=per rettangol
 if figura==1:
     lato=float(input("inserisci il valore del lato del quadrato: "))
     p=lato*4
-    print(f"il perimetro della figura è {p}")
+    print(f"il perimetro del quadrato è {p}")
 elif figura==3:
     raggio=float(input("inserisci il raggio: "))
     p=raggio*math.pi *2
-    print(f"il perimetro della figura è {p}")
+    print(f"il perimetro del cerchio è {p}")
 elif figura==2:
     base1=float(input("inserisci la prima base: "))
     base2=float(input("inserisci la seconda base: "))
     p=(base1+base2)*2
-    print(f"il perimetro della figura è {p}")
+    print(f"il perimetro del rettangolo è {p}")
 else:
     print("la figura non è disponibile")
